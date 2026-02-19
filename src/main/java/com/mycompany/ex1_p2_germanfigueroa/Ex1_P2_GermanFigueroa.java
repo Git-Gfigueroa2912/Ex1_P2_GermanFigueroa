d2bd95f4d9566475627ed5e0fc3c51c2bac592cc
Git-Gfigueroa2912 <german_figueroa@unitec.edu>
2026-02-19 17:08:30 -0600

    Examen 1

---
+++ src/main/java/com/mycompany/ex1_p2_germanfigueroa/Ex1_P2_GermanFigueroa.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ex1_p2_germanfigueroa;
+
+/**
+ *
+ * @author gafm2
+ */
+public class Ex1_P2_GermanFigueroa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
